81ab7826fca2549d04df00fa1876f114f9714123
William Duong <35279699+w-duong@users.noreply.github.com>
2019-02-21 12:30:36 -0800

    Add files via upload

--- bookinfo.cpp
+++ bookinfo.cpp
@@ -5,7 +5,7 @@
 #include "bookinfo.h"
 #include <string>
 
-void bookInfo (const string bookTitle [DBSIZE], const string isbn [DBSIZE], const string author [DBSIZE], const string pubisher [DBSIZE], const string date [DBSIZE], const int qty [DBSIZE], const double wholesale [DBSIZE], const double retail [DBSIZE], int bookIndex)
+void bookInfo (const string bookTitle [DBSIZE], const string isbn [DBSIZE], const string author [DBSIZE], const string publisher [DBSIZE], const string date [DBSIZE], const int qtyOnHand [DBSIZE], const double wholesale [DBSIZE], const double retail [DBSIZE], int bookIndex)
 {
   system ("CLS");
 
@@ -13,17 +13,15 @@ void bookInfo (const string bookTitle [DBSIZE], const string isbn [DBSIZE], cons
   cout << setw (26) << setfill (' ') << right << "Serendipity Booksellers" << endl;
   cout << setw (20) << setfill (' ') << right << "Book Information" << endl;
   cout << endl;
-  cout << "ISBN: " << isbn [bookIndex] << endl
-       << "Title: " << bookTitle [bookIndex] << endl
-       << "Author: " << author [bookIndex] << endl
-       << "Publisher: " << publisher [bookIndex] << endl
-       << "Date Added: " << date [bookIndex] << endl
-       << "Quantity-On-Hand: " << qtyOnHand [bookIndex] << endl
-       << "Wholesale Cost: " << wholesale [bookIndex] << endl
-       << "Retail Price: " << retail [bookIndex] << endl;
+  cout << "Book Title:                      > " << bookTitle [bookIndex] << endl
+       << "ISBN:                            > " << isbn [bookIndex] << endl
+       << "Author:                          > " << author [bookIndex] << endl
+       << "Publisher:                       > " << publisher [bookIndex] << endl
+       << "Date Added:                      > " << date [bookIndex] << endl
+       << "Quantity-On-Hand:                > " << qtyOnHand [bookIndex] << endl
+       << "Wholesale Cost:                  > " << wholesale [bookIndex] << endl
+       << "Retail Price:                    > " << retail [bookIndex] << endl;
   cout << endl;
-
-  cout << "Return any key to go back to previous menu." << endl;
 
   system ("pause");
 }
